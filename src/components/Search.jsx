725d67229ca49f2acb7f7195256f116389912eaf
mowan522 <shiyunwang522@gmail.com>
2022-10-27 10:46:07 +0900

    first commit

---
+++ src/components/Search.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import styled from 'styled-components';
+import {useState} from 'react';
+import {FaSearch} from 'react-icons/fa'
+import {useNavigate} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+
+function Search() {
+        // get the input text
+        const [input, setInput] = useState("");
+        const navigate = useNavigate();
+
+        //enter and load up another page
+        const submitHandler = (e) => {
+            //other components will stay intact
+            e.preventDefault();
+            navigate('/searched/' + input)
+        };
+
+  return (
+    <FormStyle onSubmit = {submitHandler}>
+        <div>
+            <FaSearch></FaSearch>
+            <input onChange={(e) => setInput(e.target.value)} 
+                    type="text"    
+                    value={input}/>
+            {/* <h1>{input}</h1> */}
+
+        </div>
+    </FormStyle>
+  )
+  }
+
+const FormStyle = styled.form`
+    margin: 0rem 20rem;
+    
+    div{
+        position: relative;
+        width: 100%;
+    }
+    input {
+        border: none;
+        width:100%;
+        background: linear-gradient(35deg, #494949, #313131);
+        font-size: 1.5rem;
+        color: white;
+        padding: 1rem 3rem;
+        border-radius: 1rem;
+        outline:none;
+    }
+
+    svg{
+        position:absolute;
+        top: 50%;
+        left: 0%;
+        transform: translate(100%, -50%);
+        color:white;
+    } 
+`
+
+export default Search
